b88376bd5b881514adf9fc87782ff0abb3694742
Nirmalkamesh027 <nirmalkamesh027@gmail.com>
2023-05-23 15:48:29 +0530

    Structure

---
+++ src/org/set/Sets.java
@@ -0,0 +1,20 @@
+package org.set;
+
+import java.util.*;
+
+public class Sets {
+	public static void main(String[] args) {
+		Set <Integer> s=new LinkedHashSet();
+		//add
+		s.add(10);
+		s.add(20);
+		s.add(30);
+		s.add(40);
+	System.out.println(s);
+		//length
+	int size = s.size();
+	System.out.println(size);
+
+	
+	}
+	}
